76966dfc263d72ab0a92b719d9f6a1ecb7be4af2
Amarachi <nwabueze2amarachi@gmail.com>
2024-07-29 17:35:03 +0100

    created a dummy db array and added routes to get and add students

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const mystudents = [
+    {id: 1, name: "amara", age: 19, email: "amara@gmail.com"},
+    {id: 2, name: "amara", age: 16, email: "amara@gmail.com"},
+    {id: 3, name: "amara", age: 12, email: "amara@gmail.com"},
+    {id: 4, name: "amara", age: 15, email: "amara@gmail.com"}
+];
+app.use(express.json())
+// get all students
+app.get("/students", (req, res) =>{
+    res.json(mystudents)
+})
+
+// add student
+app.post("/add", (req, res) =>{
+    const student = req.body;
+    mystudents.push(student);
+    res.status(201).json({
+        message: 'Student added successfully',
+        student: student
+    });
+    // res.send(`student has been added with details ${body}`);
+    console.log(student);
+});
+// get student by id
+app.get("/students/:id", (req, res) =>{
+    const params = req.params.id;
+    console.log(params);
+    res.json(params);
+})
+app.put("/update/:id", (req, res) =>{
+    const student = req.body;
+
+})
+
+
 app.listen(PORT, (req, res) => {
-    console.log(`app is running on ${PORT}`)
+    console.log(`app is running on ${PORT}`);
 })
